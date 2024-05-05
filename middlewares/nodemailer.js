f11563d41fcdf26590f24e406dd0b578f2c84674
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-05-05 19:20:51 +0200

    app done
    fix issues with email sending

--- middlewares/nodemailer.js
+++ middlewares/nodemailer.js
@@ -1,14 +1,13 @@
 const nodemailer = require("nodemailer");
 const mg = require("nodemailer-mailgun-transport");
 require("dotenv").config();
-const API_KEY  = process.env.MAILGUN_API_KEY;
+const API_KEY = process.env.MAILGUN_API_KEY;
 
 const auth = {
   auth: {
     api_key: API_KEY,
     domain: "sandboxb6ee5da89ed34859ab0dee94b0c38851.mailgun.org",
   },
-  host: "api.eu.mailgun.net",
 };
 
 const nodemailerMailgun = nodemailer.createTransport(mg(auth));
@@ -16,19 +15,17 @@ const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 const sendEmail = (to, token) => {
   nodemailerMailgun.sendMail(
     {
-      from: "m-kusmierczak@o2.pl",
-      to: to,
+      from: '<mailgun@sandbox-123.mailgun.org>',
+      to: [to],
       subject: "Welcome in our fun database",
       html: `<p>To confirm your registration, please click on the link below</p>
   <a href="http://localhost:3000/api/users/verify/${token}">Click me</a>`,
       text: `To confirm your registration, please go to :
   http://localhost:3000/api/users/verify/${token}`,
     },
-    (err, info) => {
+    (err) => {
       if (err) {
         console.log(`Error: ${err}`);
-      } else {
-        console.log(`Response: ${info}`);
       }
     }
   );
